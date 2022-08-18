cedcd76da2554fae72d0b4d3f305ab179b25110a
SG_Demi <stevengoldbergm@gmail.com>
2022-08-18 16:31:36 -0500

    Finished generator after much grief.

--- assets/js/script3.js
+++ assets/js/script3.js
@@ -1,106 +1,113 @@
-var lCase = false
-var uCase = false
-var nCase = false
-var sCase = false
-
-var letters = 'abcdefghijklmnopqrstuvwxyz'
-var randlet = letters[Math.floor(Math.random() * letters.length)]
-console.log(letters[Math.floor(Math.random() * letters.length)])
-
-var capLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-var randcap = capLetters[Math.floor(Math.random() * capLetters.length)]
-console.log(capLetters[Math.floor(Math.random() * capLetters.length)])
-
-var ints = "1234567890"
-var randint = ints[Math.floor(Math.random() * ints.length)]
-console.log(ints[Math.floor(Math.random() * ints.length)])
-
-var special = "!@#$%^&*()-=_+"
-var randspc = special[Math.floor(Math.random() * special.length)]
-console.log(special[Math.floor(Math.random() * special.length)])
-
-var passCat = ["letters", "capLetters", "ints", "special"]
-
-var passContent = []
-var password = ""
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
 
 // WHEN I click the button to generate a password
 // THEN I am presented with a series of prompts for password criteria
 // This will be easier to do in HTML/CSS. Do it last.
-// Center a flexbox in a main section of the body. Add a user form. Connext the button on the form to the script here.
+// Center a flexbox in a main section of the body. Add a user form. Connext the button on the form to the script here. 
+// event listener "click"
+
+var finalPassword = ""
+
+// Start function:
+function generatePassword() {
+    // Set up variablez
+    var lCase = false;
+    var uCase = false;
+    var nCase = false;
+    var sCase = false;
+    var password = "";
+    var passCat = ["letters", "capLetters", "ints", "special"];
+    var passContent = [];
 
 
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-window.alert("I'm glad to hear you're taking your security seriously. \n Let me ask you some additional questions. . .");
+    // test random variable properties
+    var letters = 'abcdefghijklmnopqrstuvwxyz';
+    var randlet = letters[Math.floor(Math.random() * letters.length)];
+    console.log(letters[Math.floor(Math.random() * letters.length)]);
 
+    var capLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var randcap = capLetters[Math.floor(Math.random() * capLetters.length)];
+    console.log(capLetters[Math.floor(Math.random() * capLetters.length)]);
 
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
+    var ints = "1234567890";
+    var randint = ints[Math.floor(Math.random() * ints.length)];
+    console.log(ints[Math.floor(Math.random() * ints.length)]);
 
-passLength = window.prompt("How long should your new password be? Please choose a number between 8 and 128 characters.");
-console.log("Chosen passLength: " + passLength);
-// Wrap everything in a function or Return will break things.
-function checkLength() {
+    var special = "!@#$%^&*()-=_+";
+    var randspc = special[Math.floor(Math.random() * special.length)];
+    console.log(special[Math.floor(Math.random() * special.length)]);
+
+    // WHEN prompted for password criteria
+    // THEN I select which criteria to include in the password
+    window.alert("I'm glad to hear you're taking your security seriously. \n Let me ask you some additional questions. . .");
+
+    // WHEN prompted for the length of the password
+    // THEN I choose a length of at least 8 characters and no more than 128 characters
+    passLength = window.prompt("How long should your new password be? Please choose a number between 8 and 128 characters.");
+    console.log("Chosen passLength: " + passLength);
+    console.log(isNaN(passLength))
+    
+    // Write kickback for if the user uses a non-number
+    if ((isNaN(passLength) == true) || (passLength == null)) {
+        alert("This is not a number. Please try again");
+        return;
+    }
+
+    // Write kickback for if user goes out of suggested length parameters
     if ((passLength < 8) || (passLength > 128)) {
         window.alert("That password is out of bounds. Try again with an appropriate length.");
         console.log("Bad passLength " + passLength);
         return;
     }
 
-    // Write kickback for if the user uses a non-number
-    if (Number.isNaN(passLength)) {
-        alert("This is not a number.")
-        return null;
+    // WHEN asked for character types to include in the password
+    // THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+    if (window.confirm("Should your password contain lower-case letters?")) {
+        lCase = true;
+        console.log("lCase:" + lCase);
+        passContent.push("letters");
+    } else {
+        console.log("lCase:" + lCase);
     }
-}
 
+    if (window.confirm("Should your password contain upper-case letters?")) {
+        uCase = true;
+        console.log("uCase:" + uCase);
+        passContent.push("capLetters");
+    } else {
+        console.log("uCase:" + uCase);
+    }
 
-// WHEN asked for character types to include in the password
-// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-if (window.confirm("Should your password contain lower-case letters?")) {
-    lCase = true;
-    console.log("lCase:" + lCase);
-    passContent.push("letters");
-} else {
-    console.log("lCase:" + lCase);
-}
+    if (window.confirm("What about numbers? Does you password require those?")) {
+        nCase = true;
+        console.log("nCase:" + nCase);
+        passContent.push("ints");
+    } else {
+        console.log("nCase:" + nCase);
+    }
 
-if (window.confirm("Should your password contain upper-case letters?")) {
-    uCase = true;
-    console.log("uCase:" + uCase);
-    passContent.push("capLetters");
-} else {
-    console.log("uCase:" + uCase);
-}
+    if (window.confirm("And special characters? Does you password require those?")) {
+        sCase = true;
+        console.log("sCase:" + sCase);
+        passContent.push("special");
+    } else {
+        console.log("sCase:" + sCase);
+    }
 
-if (window.confirm("What about numbers? Does you password require those?")) {
-    nCase = true;
-    console.log("nCase:" + nCase);
-    passContent.push("ints");
-} else {
-    console.log("nCase:" + nCase);
-}
+    // Write kickback for if all password fields are false
+    if ((lCase == false) && (uCase == false) && (nCase == false) && (sCase == false)) {
+        alert("Your password has no active character classes. \n Please try again.")
+        return;
+    } 
 
-if (window.confirm("And special characters? Does you password require those?")) {
-    sCase = true;
-    console.log("sCase:" + sCase);
-    passContent.push("special");
-} else {
-    console.log("sCase:" + sCase);
-}
-
-
-
-// console.log(passContent[0])
-// var randchoice = passContent[Math.floor(Math.random() * passContent.length)];
-// console.log(randchoice)
-
-numGenerator()
-function numGenerator() {
+    // Start the randomization code for password attributes.
     for (var i = 0; i < passLength - passContent.length; i++) {
 
         var randchoice = passContent[Math.floor(Math.random() * passContent.length)];
+
+        console.log("passcont " + passContent);
+        console.log("randchoice " + randchoice);
     
         if (randchoice == "letters") {
             randlet = letters[Math.floor(Math.random() * letters.length)];
@@ -122,14 +129,14 @@ function numGenerator() {
 
     console.log(password)
 
-//  generate frontBack and check for specific characters to add the last 1-4 digits.
+ //  Verify all character classes have at least 1 entry in the string - Generate frontBack to randomize which side of the string is appended
     var frontBack = Math.floor(Math.random() * 2)
 
         // Check lCase
         if (lCase === true) {
             randlet = letters[Math.floor(Math.random() * letters.length)];
-            frontBack = Math.floor(Math.random() * 2) + 1
-            console.log(frontBack)
+            frontBack = Math.floor(Math.random() * 2) + 1;
+            console.log("frontBack: " + frontBack);
         
             if (frontBack == 1) {
                 password = password + randlet;
@@ -138,14 +145,11 @@ function numGenerator() {
                 password = randlet + password;
             } 
         }
-        else {
-            lCase = false;
-        }
 
         // Check uCase
         if (uCase === true) {
             randcap = capLetters[Math.floor(Math.random() * capLetters.length)];
-            frontBack = Math.floor(Math.random() * 2) + 1
+            frontBack = Math.floor(Math.random() * 2) + 1;
 
             if (frontBack == 1) {
                 password = password + randcap;
@@ -154,14 +158,11 @@ function numGenerator() {
                 password = randcap + password;
             }
         } 
-        else {
-            uCase = false
-        }
         
         // function addNCase() {
             if (nCase === true) {
                 randint = ints[Math.floor(Math.random() * ints.length)];
-                frontBack = Math.floor(Math.random() * 2) + 1
+                frontBack = Math.floor(Math.random() * 2) + 1;
     
                 if (frontBack == 1) {
                     password = password + randint;
@@ -170,14 +171,11 @@ function numGenerator() {
                     password = randint + password;
                 }
             } 
-            else {
-                nCase = false
-            }
 
         // add sCase
         if (sCase === true) {
             randspc = special[Math.floor(Math.random() * special.length)];
-            frontBack = Math.floor(Math.random() * 2) + 1
+            frontBack = Math.floor(Math.random() * 2) + 1;
             if (frontBack == 1) {
                 password = password + randspc;
             } 
@@ -185,43 +183,20 @@ function numGenerator() {
                 password = randspc + password;
             }
         } 
-        else {
-            sCase = false
-        }
+        console.log(password)
+        finalPassword = password
+        console.log(finalPassword)
     }
 
+   
 
-
-
-// function addUCase() {
-        // if (uCase = true) {
-        //     randcap = capLetters[Math.floor(Math.random() * capLetters.length)];
-        //     frontBack = Math.floor(Math.random() * 2) + 1
-
-        //     if (frontBack == 1) {
-        //         password = password + randcap;
-        //     } 
-        //     else {
-        //         password = randcap + password;
-        //     }
-        // } else {
-        //     uCase = false
-        // }
-    // }
-
- 
-    // }
-    // function addSCase() {
-        
-    // }
-
-    // addLCase()
-    // addNCase()
-    // addSCase()
-    // addUCase()
-    console.log(password)
-// }
-
-// YES! YES! YES!
-
-// }
+// Write password to the #password input
+function writePassword() {
+    generatePassword();
+    var passwordText = document.querySelector("#password");
+  
+    passwordText.value = finalPassword;
+  }
+  
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
